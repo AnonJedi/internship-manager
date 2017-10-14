968739cf74911fc4ddb18e1e9ddf876e7d680eaa
Viktor Pisarenko <xperia.v.j.p@gmail.com>
2017-10-14 13:23:37 +0600

    Add docker

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const indexRoutes = require('./routing/index');
+const path = require('path');
+const morgan = require('morgan');
+
+app.set('view engine', 'jade');
+app.use(morgan('dev'));
+
+app.use('/', indexRoutes);
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Manager has been started on localhost:${port}`);
+});
